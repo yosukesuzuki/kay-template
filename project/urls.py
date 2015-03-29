fe1a2e3e871c741cec0a991fb14d9eaf6f82af15
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2015-03-29 14:14:43 +0900

    fix urls.py indent

--- project/urls.py
+++ project/urls.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from kay.routing import (
-  ViewGroup, Rule
+    ViewGroup, Rule
 )
 
 view_groups = [
-  ViewGroup(
-    Rule('/_ah/queue/deferred', endpoint='deferred',
-         view='kay.handlers.task.task_handler'),
-    Rule('/maintenance_page', endpoint='_internal/maintenance_page',
-         view='kay._internal.views.maintenance_page'),
-  )
+    ViewGroup(
+        Rule('/_ah/queue/deferred', endpoint='deferred',
+             view='kay.handlers.task.task_handler'),
+        Rule('/maintenance_page', endpoint='_internal/maintenance_page',
+             view='kay._internal.views.maintenance_page'),
+    )
 ]
