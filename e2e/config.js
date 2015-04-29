204590076aebb9cd202fc6e9a2aeca94afb0f153
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2015-04-29 16:38:44 +0900

    change browser to firefox

--- e2e/config.js
+++ e2e/config.js
@@ -7,7 +7,7 @@ exports.config = {
   //seleniumServerJar: '../node_modules/gulp-protractor/node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
 
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'firefox'
   },
 
   framework: 'jasmine',
